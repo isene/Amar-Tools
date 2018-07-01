7eb802c27201a2a3504f625af4442fb273d82a1c
Geir Isene <g@isene.com>
2018-07-01 23:27:08 +0200

    Added the O6 interface and better html forms

---
+++ cgi-bin/O6.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+print "Content-type: text/html\n\n"
+print <<HTML1
+<html>
+<head>
+<title>Open ended D6 rolls</title>
+</head>
+
+<body bgcolor="#FFFFFF" text="#000000">
+
+<table width="600" border="0" cellspacing="0" cellpadding="2" align="center">
+<tr><td><center><h1>Open ended D6 rolls (O6):</h1></td></tr>
+HTML1
+
+10.times {puts "<tr><td><center><h2>" + `./O6` + "</h2></center></td></tr>"}
+  
+print <<HTML2
+  <tr>                                                                                                                                                       
+    <td align="center">                                                                                                                                      
+      <form name="new" method="post" action="/cgi-bin/O6.rb">                                                                                    
+      <input type="submit" name="new" value=" Make new rolls "></form>                                                                                 
+      <p><button onclick="location.href = '/amar.html';" > Front page </button></p>                                                                          
+    </td>                                                                                                                                                    
+  </tr>                                                                                                                                                      
+  <tr><td><center><br />See the <a href="http://d6gaming.org/index.php/The_Character#Open_Ended_Rolls">Amar RPG wiki</a> for details on what an open ended d6 roll is.</td></tr>
+  <tr><td><center><small>This <a href="http://isene.org/x/O6.f95">program</a> is written in Fortran by Geir Isene.</small></center></td></tr>
+</table>
+</body>
+</html>
+HTML2
--- cli_name_gen.rb
+++ cli_name_gen.rb
@@ -3,6 +3,8 @@
 def name_gen
 
 	n = ["Human male", "Human female", "Dwarven male", "Dwarven female", "Elven male", "Elven female", "Lizardfolk", "Troll", "Araxi"]
+	f = ["human_male_first.txt", "human_female_first.txt", "dwarven_male.txt", "dwarven_female.txt", "elven_male.txt", "elven_female.txt", "lizardfolk.txt", "troll.txt", "araxi.txt"]
+
   # Get name type
 	puts "\nEnter name type:"
 	n.each_with_index do |value,index|
@@ -11,35 +13,16 @@ def name_gen
 	print "\n> "
 	c = gets.chomp.to_i
 	c = 0 if c < 0 or c > 8
-	case c
-	  when 0
-		name_file = "human_male_first.txt"
-	  when 1
-		name_file = "human_female_first.txt"
-	  when 2
-		name_file = "dwarven_male.txt"
-	  when 3
-		name_file = "dwarven_female.txt"
-	  when 4
-		name_file = "elven_male.txt"
-	  when 5
-		name_file = "elven_female.txt"
-	  when 6
-		name_file = "lizardfolk.txt"
-	  when 7
-		name_file = "troll.txt"
-	  when 8
-		name_file = "araxi.txt"
-	end
 
 	puts n[c] + " names:"
 
 	10.times do
-	  if /human/ =~ name_file
-		print `name_generator/name_generator_main.rb -d #{name_file}`.chomp + " "
-		print `name_generator/name_generator_main.rb -d human_last.txt`
+	  print `name_generator/name_generator_main.rb -d #{f[c]}`.chomp + " "
+	  if /human/ =~ f[c]
+		print `name_generator/name_generator_main.rb -d human_last.txt`.chomp
 	  else
-		puts `name_generator/name_generator_main.rb -d #{name_file}`
+		print `name_generator/name_generator_main.rb -d #{f[c]}`.chomp
 	  end
+	  print "\n"
 	end
 end
