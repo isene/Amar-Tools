b0205451c8b92b2a8446bbaf2048b8025bbf67c5
Geir Isene <g@isene.com>
2018-07-10 01:28:54 +0200

    -f

--- cgi-bin/html_town_input.rb
+++ cgi-bin/html_town_input.rb
@@ -2,21 +2,9 @@
 
 require "erb"
 
-$Level = 0 if $Level == nil
-$Day = 1 if $Day == nil
-$Terrain = 0 if $Terraintype == nil
-$Terraintype = 8 if $Terraintype == nil
-
 load "/var/www/isene.org/html/includes/includes.rb"
 
-tmpl = File.read("../enc_input.html")
-
-@sorted = Array.new
-$Encounters.each_key do |key|
-  @sorted.push(key)
-end
-
-@sorted.sort!
+tmpl = File.read("../town_input.html")
 
 out = ERB.new(tmpl)
 
