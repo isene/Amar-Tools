a22b890895a3d1d2141020cd1804abc8221430ea
Geir Isene <g@isene.com>
2018-07-09 22:02:08 +0200

    -f

--- cgi-bin/html_town_output.rb
+++ cgi-bin/html_town_output.rb
@@ -10,7 +10,7 @@ tmpl = File.read("../town_output.html")
 
 @town_size = cgi["town_size"].to_i
 @town_var  = cgi["town_var"].to_i
-aTOWN = Town.new(@type, @enc_number)
+aTOWN = Town.new(@town_size, @town_var)
 @t = aTOWN.town
 
 out = ERB.new(tmpl)
