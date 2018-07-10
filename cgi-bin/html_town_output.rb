42be5669fd141d32c36ded09bc5ab21912859b83
Geir Isene <g@isene.com>
2018-07-10 14:18:43 +0200

    Fixing web interface to Town Generator

--- cgi-bin/html_town_output.rb
+++ cgi-bin/html_town_output.rb
@@ -29,12 +29,11 @@ end
 f += "###############################################################################\n\n"
 
 begin
-	File.delete("npcs/town.npc")
-	File.open("npcs/town.npc", File::CREAT|File::EXCL|File::RDWR, 0644) do |fl|
+	File.delete("/npcs/town.npc")
+	File.open("/npcs/town.npc", File::CREAT|File::EXCL|File::RDWR, 0644) do |fl|
 		fl.write f
 	end
 rescue
-	puts "Error writing file \"npcs/town.npc\""
 end
 system("#{$editor} npcs/town.npc")
 # End: From the CLI module
