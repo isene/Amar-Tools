83f487313cae4649170030de5eae7d339b44e0e1
Geir Isene <g@isene.com>
2018-07-10 14:17:19 +0200

    Fixing web interface to Town Generator

--- cgi-bin/html_town_output.rb
+++ cgi-bin/html_town_output.rb
@@ -15,6 +15,30 @@ tmpl = File.read("../town_output.html")
 aTOWN = Town.new(@town_size, @town_var)
 @t = aTOWN.town
 
+# Start: From the CLI module
+f = "#################################<By NPCg 0.5>#################################\n\n"
+
+t.length.times do |house|
+	f += "##{house + 1}: #{t[house][0]}\n"
+	t[house][1..-1].each do |r|
+		f += "   #{r}\n"
+	end
+	f += "\n"
+end
+	
+f += "###############################################################################\n\n"
+
+begin
+	File.delete("npcs/town.npc")
+	File.open("npcs/town.npc", File::CREAT|File::EXCL|File::RDWR, 0644) do |fl|
+		fl.write f
+	end
+rescue
+	puts "Error writing file \"npcs/town.npc\""
+end
+system("#{$editor} npcs/town.npc")
+# End: From the CLI module
+
 out = ERB.new(tmpl)
 
 print "Content-type: text/html\n\n"
