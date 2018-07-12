ce2f54b998af459264647b5f405c54844a518ea3
Geir Isene <g@isene.com>
2018-07-13 01:29:37 +0200

    Minor fix

--- cgi-bin/html_town_relations.rb
+++ cgi-bin/html_town_relations.rb
@@ -8,7 +8,7 @@ cgi = CGI.new
 @town = cgi.params["upfile"][0].read
 File.write("town.txt", @town)
 
-town_relations("/cgi-bin/town.txt")
+town_relations("town.txt")
 
 tmpl = File.read("../town_relations_output.html")
 
