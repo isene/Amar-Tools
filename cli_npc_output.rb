cb45a106a1aea242d9fda92ea6a7c07f49624494
Geir Isene <g@isene.com>
2018-07-15 13:52:44 +0200

    Bug fix

--- cli_npc_output.rb
+++ cli_npc_output.rb
@@ -266,13 +266,13 @@ def npc_output(n, cli)
   f += "#######################################################################"
 
 	cli == "cli" ? tfile = "npcs/temp.npc" : tfile = "npc.txt"
-	begin
-		File.delete(tfile) if File.exists?(tfile)
-		File.write(tfile, f, perm: 0644)
-	rescue
-		puts "Error writing file #{tfile}"
-		gets if cli == "cli"
-	end
+ #begin
+ #	File.delete(tfile) if File.exists?(tfile)
+ #	File.write(tfile, f, perm: 0644)
+ #rescue
+ #	puts "Error writing file #{tfile}"
+ #	gets if cli == "cli"
+ #end
 	#system("#{$editor} #{tfile}") if cli == "cli"
 
 end
