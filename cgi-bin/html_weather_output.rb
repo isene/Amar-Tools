be8fad7e4fb04d04c070d4fbffd718fa30fa1ac3
Geir Isene <g@isene.com>
2018-07-17 18:48:17 +0200

    Started work on the web interface for the weather module

--- cgi-bin/html_weather_output.rb
+++ cgi-bin/html_weather_output.rb
@@ -2,13 +2,11 @@
 
 require "cgi"
 require "erb"
-require "date"
 
 load "../includes/includes.rb"
-load "../cli_enc_output.rb"
 
 cgi = CGI.new
-tmpl = File.read("../enc_output.html")
+tmpl = File.read("../weather_output.html")
 
 $month_n = cgi["month"].to_i
 $weather_n = cgi["weather"].to_i
