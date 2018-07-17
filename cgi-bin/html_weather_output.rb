7cd53c00b373258c47b49fd3eb64bb4175e3961d
Geir Isene <g@isene.com>
2018-07-17 18:57:12 +0200

    Started work on the web interface for the weather module

--- cgi-bin/html_weather_output.rb
+++ cgi-bin/html_weather_output.rb
@@ -11,6 +11,7 @@ tmpl = File.read("../weather_output.html")
 $month_n = cgi["month"].to_i
 $weather_n = cgi["weather"].to_i
 $wind_n = cgi["twind"].to_i
+$bgc = "#aa44cc"
 
 w = Weather_month.new($month_n, $weather_n, $wind_n)
 
