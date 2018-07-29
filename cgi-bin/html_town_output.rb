bfdb42f8255825fd5e3c35c4a49de2bbb3a52b24
Geir Isene <g@isene.com>
2018-07-29 18:55:20 +0200

    Cleanup of code, reorganizing files for less clutter.

--- cgi-bin/html_town_output.rb
+++ cgi-bin/html_town_output.rb
@@ -4,6 +4,7 @@ require "cgi"
 require "erb"
 
 load "../includes/includes.rb"
+load "../cli_town_output.rb"
 
 cgi = CGI.new
 tmpl = File.read("../town_output.html")
@@ -14,15 +15,12 @@ tmpl = File.read("../town_output.html")
 @town_var  = cgi["town_var"].to_i
 @town_name = cgi["town_name"].to_s
 aTOWN = Town.new(@town_name, @town_size, @town_var)
-@t = aTOWN.town
-@tn= ""
-
 # Use cli output function to write the town text file
-#town_output(aTOWN, "web")
+town_output(aTOWN, "web")
 
 # Generate a relationship map
-#town_relations($nfile)
-#town_dot2txt($nfile)
+town_relations($nfile)
+town_dot2txt($nfile)
 
 out = ERB.new(tmpl)
 
