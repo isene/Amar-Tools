5aef0c13afa47ca29562e44962ba6c094c047afc
Geir Isene <g@isene.com>
2018-07-29 18:50:19 +0200

    Cleanup of code, reorganizing files for less clutter.

--- cli_town_output.rb
+++ cli_town_output.rb
@@ -7,30 +7,30 @@ def town_output(aTOWN, cli)
 	f += "(You may want to bookmark this URL for future references to this Town)\n\n" unless cli == "cli"
 	f += "############################<By Amar Tools>############################\n\n"
 
-	case aTOWN.town_size
-	when 1..4
-		@tn += "Castle"
-	when 5..25
-		@tn += "Village"
-	when 26..99
-		@tn += "Town"
-	else
-		@tn += "City"
-	end
-	@tn += " Of #{aTOWN.town_name}"
-	town_name = @tn.delete(' ')
-	@tn += " - Houses: #{aTOWN.town_size} - Residents: #{aTOWN.town_residents}\n\n"
-	f   += @tn
-
-	@t.length.times do |house|
-		f += "##{house + 1}: #{@t[house][0]}\n"
-		@t[house][1..-1].each do |r|
-			f += "   #{r}\n"
-		end
-		f += "\n"
-	end
-		
-	f += "\n#######################################################################"
+#	case aTOWN.town_size
+#	when 1..4
+#		@tn += "Castle"
+#	when 5..25
+#		@tn += "Village"
+#	when 26..99
+#		@tn += "Town"
+#	else
+#		@tn += "City"
+#	end
+#	@tn += " Of #{aTOWN.town_name}"
+#	town_name = @tn.delete(' ')
+#	@tn += " - Houses: #{aTOWN.town_size} - Residents: #{aTOWN.town_residents}\n\n"
+#	f   += @tn
+#
+#	@t.length.times do |house|
+#		f += "##{house + 1}: #{@t[house][0]}\n"
+#		@t[house][1..-1].each do |r|
+#			f += "   #{r}\n"
+#		end
+#		f += "\n"
+#	end
+#		
+#	f += "\n#######################################################################"
 
 	# from functions.rb
 	#save_temp_file(f, "town", cli)
