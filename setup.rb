78312e43fd4b4be6a075a2d52d10fbe4f6f7b91a
Geir Isene <g@isene.com>
2018-07-31 08:52:32 +0200

    Final move from 'npcg' to 'amar'

--- setup.rb
+++ setup.rb
@@ -1,8 +1,8 @@
 #! /usr/bin/ruby
 
 # This is the setup program for Amar Tools.
-# It must be run as root. After running it, you 
-# can start Amar Tools via the command "npcg"
+# It must be run as root from this directory. 
+# After running it, you can start Amar Tools via the command "amar"
 
-File.symlink(File.expand_path(__FILE__), /usr/bin/amar)
+File.symlink(File.expand_path("amar.rb"), "/usr/bin/amar")
 
