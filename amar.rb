e18de99415a0b87188710c520913dc204604ab7b
Geir Isene <g@isene.com>
2020-11-10 16:45:46 +0100

    Update amar.rb

--- amar.rb
+++ amar.rb
@@ -1,7 +1,7 @@
 #! /usr/bin/ruby
 #encoding: utf-8
 
-# Welcome to Amar Tools (https://github.com/isene/npcg)
+# Welcome to Amar Tools (https://github.com/isene/Amar-Tools)
 # 
 # This file is the umbrella for Amar Tools to work as CLI/terminal program.
 
