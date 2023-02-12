fdbb73199c52b20b0a5491a1e2229c1234221b42
Geir Isene <g@isene.com>
2023-02-12 19:20:26 +0100

    Fixed bug in returning Araxi names

--- includes/tables/names.rb
+++ includes/tables/names.rb
@@ -2,7 +2,7 @@
 
 $Names = Array.new
 
-# Index         Indicator         File: first name          File: last name      Sex   Matches
+# Index         Indicator         File: first name          File: last name      Sex      Matches
 $Names[0]  = [ "Human male",     "human_male_first.txt",   "human_last.txt",     "M",    "human, half-elf, half-giant" ]
 $Names[1]  = [ "Human female",   "human_female_first.txt", "human_last.txt",     "F",    "human, half-elf, half-giant" ]
 $Names[2]  = [ "Dwarven male",   "dwarven_male.txt",       "dwarven_male.txt",   "M",    "dwarf, dwarven" ]
@@ -11,7 +11,7 @@ $Names[4]  = [ "Elven male",     "elven_male.txt",         "elven_male.txt",    
 $Names[5]  = [ "Elven female",   "elven_female.txt",       "elven_female.txt",   "F",    "elf, elven, faerie, faery, pixie, brownie, goblin" ]
 $Names[6]  = [ "Lizardfolk",     "lizardfolk.txt",         "",                   "M, F", "lizard, lizardman, lizardfolk" ]
 $Names[7]  = [ "Troll",          "troll.txt",              "",                   "M, F", "troll, lesser troll, trollkin, ogre" ]
-$Names[8]  = [ "Araxi",          "araxi.txt",              "",                   "M, F", "arax, anarax" ]
+$Names[8]  = [ "Araxi",          "araxi.txt",              "",                   "M, F", "araxi, arax, anaraxi, anarax" ]
 $Names[9]  = [ "Generic male",   "fantasy_male.txt",       "",                   "M",    "generic, fantasy, monster, other" ]
 $Names[10] = [ "Generic female", "fantasy_female.txt",     "",                   "F",    "generic, fantasy, monster, other" ]
 $Names[11] = [ "Castle",         "castle_names.txt",       "",                   "",     "castle" ]
